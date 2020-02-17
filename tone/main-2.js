ead9f99e7a4f9e05151e53c75de5aef97db6b49e
JoshuaHolloway <jhollow6@asu.edu>
2020-02-17 13:33:57 -0600

    Proper synchronization at startup

--- tone/main-2.js
+++ tone/main-2.js
@@ -8,12 +8,32 @@ let synth = new Tone.Synth().toMaster()
 const play_button = document.getElementById('play_button');
 play_button.addEventListener('click', () => {
 
+
+});
+
+
+
+
+//attach a click listener to a play button
+document.querySelector('#play_button').addEventListener('click', async () => {
+    // -The await expression causes async function execution to pause until 
+    //  a Promise is settled (that is, fulfilled or rejected), and to resume 
+    //  execution of the async function after fulfillment.
+    await Tone.start();
+    console.log('audio is ready');
+
     loopBeat = new Tone.Loop(callback, '4n');
     Tone.Transport.bpm.value = 140;
     Tone.Transport.start();
 
     loopBeat.start(0);
-});
+})
+
+
+
+
+
+
 
 const stop_button = document.getElementById('stop_button');
 stop_button.addEventListener('click', () => {
@@ -36,4 +56,15 @@ function callback(time) {
 
     //play a middle 'C' for the duration of an 8th note
     synth.triggerAttackRelease('C4', '8n', time, velocity)
+
+
+
+    const currentBeat = Tone.Transport.position;
+    const x = currentBeat.split(':');
+    console.log(x);
+    console.log(currentBeat);
+    console.log(typeof currentBeat);
+
+
+
 }
