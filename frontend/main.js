b8523429c4291e3bd6ca43ecd1fed945ad0c36ec
JoshuaHolloway <jhollow6@asu.edu>
2020-02-18 16:28:01 -0600

    turning keys on and off with click

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,5 @@
 let loopBeat;
 
-
 //create a synth and bass and connect them to the master output (your speakers)
 let bassSynth = new Tone.MembraneSynth().toMaster();
 let synth = new Tone.Synth().toMaster()
@@ -8,11 +7,7 @@ let synth = new Tone.Synth().toMaster()
 const play_button = document.getElementById('play_button');
 play_button.addEventListener('click', () => {
 
-
 });
-
-
-
 
 //attach a click listener to a play button
 document.querySelector('#play_button').addEventListener('click', async () => {
@@ -30,19 +25,13 @@ document.querySelector('#play_button').addEventListener('click', async () => {
     loopBeat.start(0);
 })
 
-
-
-
-
-
-
 const stop_button = document.getElementById('stop_button');
 stop_button.addEventListener('click', () => {
     loopBeat.stop();
 });
 
 const vol_slider = document.getElementById('volume');
-console.dir(vol_slider);
+// console.dir(vol_slider);
 
 let volume;
 vol_slider.addEventListener('change', () => {
@@ -50,31 +39,35 @@ vol_slider.addEventListener('change', () => {
     console.log('volume = ' + volume + ', type: ' + typeof volume);
 });
 
+//const N = 16;
+// let track_1 = new Array(16);
+// let track_2 = new Array(16);
+// for (let i = 0; i < N; ++i) {
+//     track_1[i] = 0;
+//     track_2[i] = 0;
+// }
+
+let track_1 = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let track_2 = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
 function callback(time) {
     const velocity = volume;
-    bassSynth.triggerAttackRelease('c1', '8n', time, velocity);
-    console.log(time);
-
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease('C4', '8n', time, velocity)
 
     const BarsBeatsSixteenths = Tone.Transport.position;
     const Bars_Beats_Sixteenths = BarsBeatsSixteenths.split(':');
-    console.log(Bars_Beats_Sixteenths);
-
     // .position ↝ BarsBeatsSixteenths #
     // The Transport’s position in Bars:Beats:Sixteenths. Setting the value will jump to that position right away.
     //      BarsBeatsSixteenths
     //      A colon-separated representation of time in the form of Bars:Beats:Sixteenths.
 
     const bar = Number(Bars_Beats_Sixteenths[0]);
-    console.log(`bar: ${bar}`);
+    // console.log(`bar: ${bar}`);
 
     const beat = Number(Bars_Beats_Sixteenths[1]);
-    console.log(`beat: ${beat}`);
+    // console.log(`beat: ${beat}`);
 
     const sixteenth = Number(Bars_Beats_Sixteenths[2]);
-    console.log(`sixteenth: ${sixteenth}`);
+    // console.log(`sixteenth: ${sixteenth}`);
 
     const bar_elem = document.querySelector('#bar');
     const beat_elem = document.querySelector('#beat');
@@ -83,7 +76,7 @@ function callback(time) {
 
     const beats_1 = document.querySelectorAll('.beats-1');
     const beats_2 = document.querySelectorAll('.beats-2');
-    console.dir(beats_1);
+    // console.dir(beats_1);
 
     beats_1[bar].style.background = 'yellow';
     beats_2[bar].style.background = 'yellow';
@@ -91,15 +84,42 @@ function callback(time) {
         beats_1[bar - 1].style.background = 'green';
         beats_2[bar - 1].style.background = 'green';
     }
+
+
+    if (track_1[bar]) {
+        bassSynth.triggerAttackRelease('c1', '8n', time, velocity);
+    }
+    if (track_2[bar]) {
+        //play a middle 'C' for the duration of an 8th note
+        synth.triggerAttackRelease('C4', '8n', time, velocity)
+    }
+
+    // console.log(time);
 }
 
 // Grab each individual beat and  
 const beat_1 = document.querySelectorAll('.beat-1');
-for (let b of beat_1) {
-    b.addEventListener('click', () => {
-        b.style.background = 'purple';
+// for (let b of beat_1) {
+//     b.addEventListener('click', () => {
+//         b.style.background = 'purple';
+//     });
+// }
+
+beat_1.forEach(function (value, i) {
+
+    beat_1[i].addEventListener('click', () => {
+
+        if (track_1[i]) beat_1[i].style.background = 'rgba(255, 154, 72)';
+        else beat_1[i].style.background = 'black';
+
+
+        track_1[i] = !track_1[i];
+        console.log(track_1[i]);
     });
-}
+
+    //console.log('%d: %s', i, value);
+});
+
 
 const beat_2 = document.querySelectorAll('.beat-2');
 for (let b of beat_2) {
