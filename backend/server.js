e4408f29e1af7276a1c09a4507a91c7cc9e1d5c6
JoshuaHolloway <jhollow6@asu.edu>
2020-02-22 22:28:54 -0600

    fullstack

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const app = express();
+//=======================================
+app.set('view engine', 'ejs');
+//=======================================
+// JS-Object
+const db = {
+  // Key: Value
+  philip: {
+    // JS-Object
+    //Key: Value
+    job: 'prof',
+    pet: 'cat.jpg'
+  },
+  josh: { job: 'dev', pet: 'dog.jpg' },
+  steve: { job: 'eng', pet: 'bear.jpg' }
+};
+//=======================================
+app.get('/josh', (req, res) => {
+    console.log('INSIDE /josh! :)');
 
-const path = require('path');
-
-// executed for every incoming request
-app.use('/', (req, res, next) => {
-    console.log('In the middleware');
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+    // Send response:
+  const db_keys = Object.keys(db);
+  res.send(db_keys);
 });
+//=======================================
+// executed for every incoming request
+app.use('/', (req, res) => {
+    console.log('Root Route Handler: /');
 
+    res.render('index.ejs', {
+        josh: 'HI from server.js :)'
+    });
+});
+//=======================================
 const port_num = 3e3;
 app.listen(port_num, () => console.log('http://localhost:3000'));
+//=======================================
