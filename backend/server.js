78191e0561ac32fe3e1e0551b0e87ab425f1e8e5
JoshuaHolloway <jhollow6@asu.edu>
2020-02-23 20:13:10 -0600

    db query

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 //=======================================
+const sqlite3 = require('sqlite3');
+const db = new sqlite3.Database('pets.db');
+//=======================================
 app.set('view engine', 'ejs');
 //=======================================
 // JS-Object
-const db = {
+const fake_db = {
   // Key: Value
   philip: {
     // JS-Object
@@ -19,9 +22,18 @@ const db = {
 app.get('/josh', (req, res) => {
     console.log('INSIDE /josh! :)');
 
-    // Send response:
-  const db_keys = Object.keys(db);
-  res.send(db_keys);
+    db.all('SELECT name FROM users_to_pets', (err, rows) => {
+        // console.log(rows);
+        const db_names = rows.map(x => x.name);
+        // console.log(db_names);
+
+        // Send response:
+        res.send(db_names);
+    });
+
+    // // Send response:
+    // const db_keys = Object.keys(fake_db);
+    // res.send(db_keys);
 });
 //=======================================
 // executed for every incoming request
